2a9623b1d2951baffe10cde1970bda0e1884fb09
Ethan McGimpsey <54158496+MrMuskyHusky@users.noreply.github.com>
2019-11-13 15:57:15 +1100

    Update #14

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -1,24 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Node : MonoBehaviour
 {
     public Color hoverColor;
     public Color clickColor;
 
+    private GameObject turret;
+
     private Renderer rend;
     private Color startColor;
+
+    BuildManager buildManager;
 
     void Start()
     {
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
+
+        buildManager = BuildManager.instance;
     }
 
     void OnMouseDown()
     {
-        rend.material.color = clickColor;
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        if (buildManager.GetTurretToBuild() == null)
+        {
+            return;
+        }
+
+        if(turret != null)
+        {
+            Debug.Log("Can't build there!");
+            rend.material.color = clickColor;
+            return;
+        }
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position, transform.rotation);
     }
 
     void OnMouseUp()
@@ -28,6 +52,15 @@ public class Node : MonoBehaviour
 
     void OnMouseEnter()
     {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        if (buildManager.GetTurretToBuild() == null)
+        {
+            return;
+        }
         rend.material.color = hoverColor;
     }
     void OnMouseExit()
